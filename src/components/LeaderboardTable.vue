5066068c8574a19abe940d59fe7cfc502ecd80f5
adamhider <ajder.adjivapov@gmail.com>
2023-02-15 00:32:31 +0300

    Leaderboard and chart

--- src/components/LeaderboardTable.vue
+++ src/components/LeaderboardTable.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <q-list>
-            <q-item v-for="(commonItem, commonKey) in leaderboard.data.common_statistics?.list" :key="commonKey" class="q-my-sm q-px-none" >
+            <q-item v-for="(commonItem, commonKey) in leaderboard.common?.list" :key="commonKey" class="q-my-sm q-px-none" >
                 <q-item-section avatar>
                     <q-avatar rounded  color="primary" text-color="white" size="50px">
                         <q-icon name="star" color="blue-6" size="50px" class="absolute-top"/>
@@ -18,6 +18,12 @@
                 </q-item-section>
             </q-item>
         </q-list>
+        <Line
+          v-if="leaderboard.chart.datasets"
+          id="my-chart-id"
+          :options="chartOptions"
+          :data="leaderboard.chart"
+        />
     </div>
 </template>
 
@@ -25,16 +31,115 @@
 import { reactive, onActivated } from 'vue'
 import { useExercise } from '../composables/useExercise.js'
 import { useUserStore } from '../stores/user'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
 
 const { user } = useUserStore()
 const { getLeaderboard } = useExercise()
 
 const leaderboard = reactive({
-  data: {}
+  common: {},
+  chart: {}
 })
 
 onActivated(async () => {
-  leaderboard.data = await getLeaderboard({ classroom_id: user.active.data.profile.classroom_id, time_period: 'month' })
+  const leaderboardResponse = await getLeaderboard({ classroom_id: user.active.data.profile.classroom_id, time_period: 'month' })
+  leaderboard.common = leaderboardResponse.common_statistics
+  leaderboard.chart = {
+    labels: leaderboardResponse.chart_statistics.dates.labels,
+    datasets: leaderboardResponse.chart_statistics.users
+  }
+  ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale)
 })
+const chartOptions = {
+  responsive: true,
+  scaleStartValue: 0,
+  animation: {
+    easing: 'easeInOutQuad',
+    duration: 520
+  },
+  plugins: {
+    legend: {
+      display: false,
+      labels: {
+        usePointStyle: true,
+        fillStyle: 'rgb(255, 99, 132)',
+        boxWidth: 40,
+        family: 'Blogger Sans'
+      }
+    },
+    tooltip: {
+      enabled: true,
+      position: 'nearest'
+    }
+  },
+  elements: {
+    line: {
+      borderWidth: 2,
+      backgroundColor: 'green'
+    }
+  },
+  scales: {
+    x: {
+      display: true,
+      grid: {
+        color: '#e3e3e3',
+        lineWidth: 1,
+        display: false
+      },
+      ticks: {
+        font: {
+          size: 13,
+          family: 'Blogger Sans'
+        }
+      }
+    },
+    y: {
+      beginAtZero: true,
+      display: true,
+      grid: {
+        color: '#e3e3e3',
+        lineWidth: 1,
+        drawBorder: false
+      },
+      ticks: {
+        padding: 10,
+        precision: 0,
+        font: {
+          size: 13,
+          family: 'Blogger Sans'
+        },
+        count: 5
+      }
+    }
+  },
+  layout: {
+  },
+  animations: {
+    y: {
+      easing: 'easeOutQuart',
+      to: (ctx) => {
+        if (ctx.type === 'data') {
+          if (ctx.mode === 'default' && ctx.dropped) {
+            ctx.dropped = false
+            return 1
+          }
+        }
+      }
+    }
+  }
+}
+if (window.innerWidth < 768) {
+  chartOptions.scales.x.display = false
+}
 
 </script>
