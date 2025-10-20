4dab4c306841ef65764acec1b6f4144f1b80272d
AdamHider <ajder.adjivapov@gmail.com>
2025-10-21 01:48:43 +0300

    Leaderboard enhanced

--- src/components/LeaderboardTable.vue
+++ src/components/LeaderboardTable.vue
@@ -1,66 +1,72 @@
 <template>
-  <q-card flat style="min-height:300px">
-    <q-inner-loading :showing="isLoading">
-      <q-spinner-puff size="50px" color="primary" />
-    </q-inner-loading>
-    <q-card-section class="q-py-sm text-left">
-      <div v-for="(winner, commonKey) in leaderboardData.head" :key="commonKey"  :class="`q-my-sm text-left rounded-sm ${(row.is_active == 1) ? ' bg-gradient-primary text-white' : 'text-dark'}`">
-        <LeaderboardTableItem :item="winner" />
-      </div>
-    </q-card-section>
-    <q-card-section class="q-py-sm text-left">
-      <LeaderboardFilter
-        :allowed-filters="props.allowedFilters"
-        :timePeriod="props.timePeriod"
-        @update-filter="updateFilter($event)"
-      />
-    </q-card-section>
-    <q-card-section v-if="leaderboardData.data.length > 0 " class="q-py-sm q-px-sm relative-position">
-      <q-list  >
-        <q-item class="text-left text-bold text-grey-7" style="font-size: 12px">
-          <q-item-section avatar>
-            <q-item-label>Место</q-item-label>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Герой</q-item-label>
-          </q-item-section>
-          <q-item-section side>
-            <q-item-label>Очки</q-item-label>
-          </q-item-section>
-        </q-item>
-        <div v-for="(row, commonKey) in leaderboardData.data" :key="commonKey" :class="`q-my-sm text-left rounded-sm ${(row.is_active == 1) ? ' bg-gradient-primary text-white' : 'text-dark'}`">
-          <LeaderboardTableItem :item="row" />
-        </div>
-      </q-list>
-    </q-card-section>
-    <q-card-section v-else class="q-pa-none">
-      <BannerNotFound
-        title="Упс..."
-        description="Пока показать нечего"
-        default-image
-      />
-    </q-card-section>
-  </q-card>
+    <q-card flat style="min-height:300px">
+      <q-card-section class="q-py-sm text-left">
+        <LeaderboardFilter
+          :allowed-filters="props.allowedFilters"
+          :timePeriod="props.timePeriod"
+          @update-filter="updateFilter($event)"
+        />
+      </q-card-section>
+      <q-card-section v-if="items.length > 0 " class="q-py-sm q-px-sm relative-position">
+
+        <q-list>
+          <q-item class="text-left text-bold text-grey-7" style="font-size: 12px">
+            <q-item-section side>
+              <q-item-label>Место</q-item-label>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Герой</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-item-label>Прогресс и очки</q-item-label>
+            </q-item-section>
+          </q-item>
+          <div v-for="(winner, winnerIndex) in winners" :key="`winnerIndex${winnerIndex}`">
+            <LeaderboardTableItem :item="winner"/>
+          </div>
+          <q-virtual-scroll
+            class="col"
+            style="height: 420px"
+            :virtual-scroll-item-size="56"
+            :items="items"
+            @virtual-scroll="onScroll"
+            ref="virtualScrollRef"
+          >
+            <template v-slot:before>
+              <div style="position: sticky; top: 0; z-index: 1;" class="bg-white">
+                <LeaderboardTableItem v-if="!state.userIsVisible && state.userIsAbove" :item="userRow" :index="-1"/>
+              </div>
+            </template>
+            <template v-slot="{ item, index }">
+              <div :style="(item.is_active) ? 'position: sticky; bottom: 0; top: 0; z-index: 1;' : ''" :index="index" :key="`itemIndex${index}`">
+                <LeaderboardTableItem :item="item" />
+              </div>
+            </template>
+            <template v-slot:after>
+              <div style="position: sticky; bottom: 0; z-index: 1;" class="bg-white">
+                <LeaderboardTableItem v-if="!state.userIsVisible && state.userIsBelow" :item="userRow" :index="-1"/>
+              </div>
+            </template>
+          </q-virtual-scroll>
+        </q-list>
+      </q-card-section>
+      <q-card-section v-else class="q-pa-none">
+        <BannerNotFound
+          title="Упс..."
+          description="Пока показать нечего"
+          default-image
+        />
+      </q-card-section>
+    </q-card>
 </template>
 
 <script setup>
-import { onActivated, onMounted, ref, reactive, nextTick } from 'vue'
-import LeaderboardFilter from '../components/LeaderboardFilter.vue'
+import { onActivated, onMounted, watch, ref, reactive, nextTick } from 'vue'
 import BannerNotFound from '../components/BannerNotFound.vue'
+import LeaderboardFilter from '../components/LeaderboardFilter.vue'
 import { useExercise } from '../composables/useExercise.js'
 import LeaderboardTableItem from '../components/LeaderboardTableItem.vue'
-
-const { getLeaderboard } = useExercise()
-
-const leaderboardData = reactive({
-  filter: {
-    time_period: 'all'
-  },
-  data: {},
-  head: {}
-})
-const isLoading = ref(false)
-
+import { api } from '../services/index'
 
 const props = defineProps({
   allowedFilters: Array,
@@ -68,34 +74,77 @@ const props = defineProps({
   challengeId: String,
   timePeriod: String
 })
+const userRowIndex = ref(0)
+const items = ref([]);
+const winners = ref([]);
+const virtualScrollRef = ref(null);
+const isLoadingInitial = ref(true);
+const userRow = ref({})
+const filter = ref({})
+const initialStartFilter = 0;
+const initialEndFilter = 0;
 
-const loadTable = async () => {
-  isLoading.value = true
-  const filter = prepareFilter()
-  const leaderboardResponse = await getLeaderboard(filter)
-  leaderboardData.data = leaderboardResponse.body
-  leaderboardData.head = leaderboardResponse.head
+const state = reactive({
+  isLoading: false,
+  userIsAbove: false,
+  userIsBelow: false,
+  userIsVisible: true
+});
 
-  isLoading.value = false
-}
+const loadInitialData = async () => {
+  state.userIsAbove = false
+  state.userIsBelow = false
+  state.isLoading = true;
+  isLoadingInitial.value = true;
 
-const prepareFilter = () => {
-  if (props.lessonId) leaderboardData.filter.lesson_id = props.lessonId
-  if (props.timePeriod) leaderboardData.filter.time_period = props.timePeriod
-  leaderboardData.filter.mode = 'new'
-  return leaderboardData.filter
-}
+  filter.value.mode = 'new'
 
-const updateFilter = (filter) => {
-  leaderboardData.filter = filter
-  loadTable()
-}
+  const initialData = await api.exercise.getLeaderboard(filter.value);
+  items.value = initialData.body;
+  userRow.value = initialData.head.user_row
+  winners.value = initialData.head.winners
+  state.isLoading = false;
 
-onActivated(async () => {
-  await loadTable();
-  await nextTick();
-  if(document.querySelector('#userRowAnchor')){
-    document.querySelector('#userRowAnchor').scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
+  if (initialData.body.length > 0) {
+    userRowIndex.value = initialData.body.findIndex(item => item.is_active);
   }
+  isLoadingInitial.value = false;
+
+  await nextTick();
+
+  if(virtualScrollRef.value){
+    virtualScrollRef.value.scrollTo(userRowIndex.value)
+  }
+};
+const updateFilter = (filterObject) => {
+  filter.value = filterObject
+  loadInitialData()
+}
+
+
+const onScroll = ({ index, from, to, direction, ref }) => {
+  state.direction = direction
+  state.userIsVisible = false
+
+  for(var i = from; i <= to; i++){
+    if(items.value[i].is_active){
+        state.userIsVisible = true
+    }
+  }
+
+};
+watch(() => state.userIsVisible, () => {
+  if(!state.userIsVisible){
+    if(state.direction == 'increase'){
+      state.userIsAbove = true
+      state.userIsBelow = false
+    } else {
+      state.userIsBelow = true
+      state.userIsAbove = false
+    }
+  }
+})
+onActivated(async () => {
+  await loadInitialData();
 })
 </script>
--- src/components/LeaderboardTableItem.vue
+++ src/components/LeaderboardTableItem.vue
@@ -1,16 +1,16 @@
 <template>
-    <q-item :active="item.is_active == 1" :class="` text-left rounded-sm ${(item.is_active == 1) ? ' bg-gradient-primary text-white' : 'bg-white text-dark'}`">
-        <q-item-section avatar class="text-center">
-          <q-avatar v-if="item.place == 1" floating rounded color="gradient-gold" class="text-white q-push" size="40px" style="box-shadow: rgba(255, 255, 255, 0.51) 0px 0px 0px 2px inset;">
+    <q-item  :active="item.is_active == 1" :class="` text-left ${(item.place <= 3) ? 'q-push q-my-xs place-'+item.place : ''} rounded-sm ${(item.is_active == 1) ? ' place-user q-push' : 'bg-white text-dark place-common'}`">
+        <q-item-section side class="text-center ">
+          <q-avatar v-if="item.place == 1" floating rounded color="gradient-gold" class="text-white q-push" size="32px" style="box-shadow: rgba(255, 255, 255, 0.51) 0px 0px 0px 2px inset;">
             <b style="margin-bottom:2px">{{ item.place }}</b>
           </q-avatar>
-          <q-avatar v-else-if="item.place == 2" floating rounded color="gradient-silver" size="40px" class="text-white q-push" style="box-shadow: rgba(255, 255, 255, 0.51) 0px 0px 0px 2px inset;">
+          <q-avatar v-else-if="item.place == 2" floating rounded color="gradient-silver" size="32px" class="text-white q-push" style="box-shadow: rgba(255, 255, 255, 0.51) 0px 0px 0px 2px inset;">
             <b style="margin-bottom:2px">{{ item.place }}</b>
           </q-avatar>
-          <q-avatar v-else-if="item.place == 3" floating rounded color="gradient-bronze" size="40px" class="text-white q-push" style="box-shadow: rgba(255, 255, 255, 0.51) 0px 0px 0px 2px inset;">
+          <q-avatar v-else-if="item.place == 3" floating rounded color="gradient-bronze" size="32px" class="text-white q-push" style="box-shadow: rgba(255, 255, 255, 0.51) 0px 0px 0px 2px inset;">
             <b style="margin-bottom:2px">{{ item.place }}</b>
           </q-avatar>
-          <q-avatar v-else size="36px" >
+          <q-avatar v-else size="28px" >
             <b>{{ item.place }}</b>
           </q-avatar>
         </q-item-section>
@@ -26,7 +26,7 @@
           <q-item-label>
             <span class="q-pa-none ">
               <b>{{ item.name }}</b>
-              <span class="q-ml-xs" v-if="item.achievements.length > 0">
+              <span class="q-ml-xs" v-if="item.achievements?.length > 0">
                 <q-avatar size="22px" v-for="(achievementItem, achievementIndex) in item.achievements" :key="`achievementIndex${achievementIndex}`">
                   <q-img :src="achievementItem.image" no-transition/>
                   <q-menu self="top middle">
@@ -43,12 +43,24 @@
               </span>
             </span>
           </q-item-label>
-          <q-item-label caption :class="` ${(item.is_active == 1) ? ' text-white' : 'text-grey-8'}`">
+          <q-item-label caption>
             Уровень {{ item.level.level }}
           </q-item-label>
         </q-item-section>
         <q-item-section side>
-          <div :class="`text-subtitle ${(item.is_active == 1) ? ' text-white' : 'text-grey-8'}`"><b>{{ item.points }}</b></div>
+          <q-avatar v-if="item.is_growing" class="q-push rounded-xs"  color="green-5" size="22px">
+            <q-icon size="14px" color="white" class="q-mb-xs" name="keyboard_double_arrow_up"/>
+          </q-avatar>
+          <q-avatar v-else class="q-push rounded-xs"  color="red-5" size="22px">
+            <q-icon size="14px" color="white" class="q-mb-xs" name="keyboard_double_arrow_down"/>
+          </q-avatar>
+        </q-item-section>
+        <q-item-section side style="padding-left: 4px">
+          <div class="stars flex items-center text-dark bg-dark-transparent rounded-sm q-px-sm q-py-xs">
+            <b style="font-size: 0.75rem;">{{ item.resource.quantity.toLocaleString() }}</b>
+            <q-img :src="item.resource.image" width="18px" height="18px" class=" star-image q-ml-xs" no-transition
+              style="flex: 1; margin-top: -2px; filter: drop-shadow(0px 1px 2px #00000075);"/>
+          </div>
         </q-item-section>
     </q-item>
 
@@ -60,3 +72,53 @@ const props = defineProps({
 })
 
 </script>
+<style scoped lang="scss">
+.star-image{
+
+}
+.place-1{
+  background: $gradient-gold !important;
+  color: white !important;
+  text-shadow: 1px 1px 3px #0000008f;
+  .stars, .text-caption, .q-avatar{
+    color: white !important;
+  }
+}
+.place-2{
+  background: $gradient-silver !important;
+  color: white !important;
+  text-shadow: 1px 1px 3px #0000008f;
+  .stars, .text-caption, .q-avatar{
+    color: white !important;
+  }
+}
+.place-3{
+  background: $gradient-bronze !important;
+  color: white !important;
+  text-shadow: 1px 1px 3px #0000008f;
+  .stars, .text-caption, .q-avatar{
+    color: white !important;
+  }
+}
+
+
+
+.place-user{
+  background: $gradient-primary !important;
+  color: white !important;
+  text-shadow: 1px 1px 3px #0000008f;
+  .stars, .text-caption, .q-avatar{
+    color: white !important;
+  }
+}
+.place-common:not(.place-1,.place-2,.place-3){
+  border-bottom: 4px solid transparent;
+  .stars{
+    background: $grey-3 !important;
+  }
+  .q-avatar{
+    color: $dark;
+  }
+}
+
+</style>
